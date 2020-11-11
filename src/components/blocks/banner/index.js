1f1eac7d910b958acabae500f82dddf81db5231c
Amy Kapernick <amy@aimhigherweb.design>
2020-11-11 16:08:17 +0800

    banner section styled

--- src/components/blocks/banner/index.js
+++ src/components/blocks/banner/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { getFixedGatsbyImage } from 'gatsby-storyblok-image';
+import { getFluidGatsbyImage } from 'gatsby-storyblok-image';
 import Img from 'gatsby-image';
 
 import Block from "../../parts/block";
 import CTA from '../../parts/cta';
 
+import './style.scss';
+
 const Banner = ({
 	main_quote, sub_quote, cta_button, image
 }) => (
-	<Block>
+	<Block className="banner">
 		<blockquote>
 			<p className="main">{main_quote}</p>
 			<p className="sub">{sub_quote}</p>
 			<CTA {...{ cta_button }} />
 		</blockquote>
-		<Img fixed={getFixedGatsbyImage(image.filename, { width: 500 })} />
+		<Img fluid={getFluidGatsbyImage(image.filename, { maxWidth: 500 })} />
 	</Block>
 );
 
--- src/components/parts/cta/index.js
+++ src/components/parts/cta/index.js
@@ -5,7 +5,7 @@ import A from '../link';
 const CTA = ({ cta_button }) => (
 	<ul>
 		{cta_button.map((cta) => (
-			<li key={cta.cta_link}><A className="cta" url={cta.cta_link}>{cta.cta_text}</A></li>
+			<li key={cta.cta_link}><A className="btn" url={cta.cta_link}>{cta.cta_text}</A></li>
 		))}
 	</ul>
 );

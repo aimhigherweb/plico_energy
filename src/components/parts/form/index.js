fa8da5464822a36ec34156d09c90965964a2df9e
Amy Kapernick <amy@aimhigherweb.design>
2020-11-24 13:16:57 +0800

    testing form change

--- src/components/parts/form/index.js
+++ src/components/parts/form/index.js
@@ -53,7 +53,6 @@ const Form = ({ form }) => (
 				<form
 					className="custom"
 					name={formData.slug}
-					method="post"
 					action="/thanks/"
 					data-netlify="true"
 					data-netlify-honeypot="bot-field"

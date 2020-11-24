d861ca7eecf95e72c6f6bfb86e421f24978d758f
Amy Kapernick <amy@aimhigherweb.design>
2020-11-24 13:20:56 +0800

    added name and bot fields

--- src/components/parts/form/index.js
+++ src/components/parts/form/index.js
@@ -58,6 +58,8 @@ const Form = ({ form }) => (
 					data-netlify="true"
 					data-netlify-honeypot="bot-field"
 				>
+					<input type="hidden" name="bot-field" />
+					<input type="hidden" name="form-name" value={formData.slug} />
 					{formData.fields.content.fields.map((field) => (
 						<Field
 							key={field._uid}

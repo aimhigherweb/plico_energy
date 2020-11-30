6b9cd791b1c3d3771cae71a4961f2c07ccaf139e
Amy Kapernick <amy@aimhigherweb.design>
2020-11-30 19:18:21 +0800

    updating form to hide static form

--- src/components/parts/content_section/index.js
+++ src/components/parts/content_section/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Blob from '../../../img/blob_outline.svg';
+import Blob from '../../../img/blob_background.svg';
 
 import './style.scss';
 
 const Section = ({ heading, content, index }) => (
-	<div className={`section ${index % 2 !== 0 && `secondary`}`}>
+	<div className={`section ${index % 2 !== 0 && `secondary`} s_${index + 1}`}>
 		<h3>{heading}</h3>
 		<div dangerouslySetInnerHTML={{ __html: content }} />
 		<Blob className="background" />

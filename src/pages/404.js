883755ab61a4197d7e20e801069ed1301ce03326
Amy Kapernick <amy@aimhigherweb.design>
2020-12-14 16:09:08 +0800

    fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,20 @@ import Layout from '../components/partials/layout';
 
 import '../styles/custom/not_found.scss';
 
-const FourOhFour = () => (
-	<Layout {...{ classes: `page` }}>
-		<h1>Page not found</h1>
-		<p>Looks like that page doesn't exist anymore or has moved. Try going back to the <a href="/">home page</a> or selecting one of the options from our menu instead.</p>
-	</Layout>
-);
+const FourOhFour = () => {
+	const meta = {
+		title: `404 - Page not Found`,
+		description: `Looks like that page doesn't exist anymore or has moved.`
+	};
+
+	return (
+		<Layout {...{ classes: `page not_found`, meta }}>
+			<div className="content">
+				<h1>404 - Page not found</h1>
+				<p>Looks like that page doesn't exist anymore or has moved. Try going back to the <a href="/">home page</a> or selecting one of the options from our menu instead.</p>
+			</div>
+		</Layout>
+	);
+};
 
 export default FourOhFour;
